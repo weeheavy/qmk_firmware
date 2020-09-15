71bc6660d0547e92690ffd697fdc28f187a4838b
Oliver Ladner <waste@lugh.ch>
2020-09-15 09:19:51 +0200

    switch up down arrows

--- keyboards/dz60/keymaps/weeheavy_2.25_lshift/keymap.c
+++ keyboards/dz60/keymaps/weeheavy_2.25_lshift/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
 	KC_BSPC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 	KC_LSFT, XXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RSFT, KC_RSFT,
-	KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, MO(L1), KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT),
+	KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, MO(L1), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT),
 
 // Utility layer - RGB and multimedia control, reset and additional "b" button
 [L1] = LAYOUT_all(
